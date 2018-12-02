395b2ae70834f9942575161547bfb3081ff4232d
James Bliss <james.bliss@lush.co.uk>
2018-12-02 15:32:04 +0000

    removing logs

--- match/resolvers.js
+++ match/resolvers.js
@@ -4,23 +4,15 @@ const Moment = require('moment');
 //
 module.exports = {
   nextMatch: async (data) => {
-    console.log('hello', data);
-
     const id = 64;
     const date = Moment(new Date());
-    console.log(data)
     const today = date.format('YYYY-MM-DD');
-    console.log(today)
     const week = date.add(1, 'M').format('YYYY-MM-DD');
-    console.log(week)
 
     const url = `https://api.football-data.org/v2/teams/${id}/matches?dateFrom=${today}&dateTo=${week}`
 
     const match = await fetch(url, { headers: { "X-Auth-Token": process.env.FOOTBALL_KEY } })
       .then(res => res.json());
-
-    console.log({match})
-    console.log({...match.matches})
 
     return match.matches[0];
   }
