61a70208756d0b4a96ad29454b772733d23ac94a
James Bliss <james.bliss@lush.co.uk>
2021-06-23 17:00:08 +0100

    this sucks

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get("*", (req, res) => {
   res.redirect("https://github.com/JamesBliss/footballQL");
 });
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.listen({ port }, () =>
   console.log(
